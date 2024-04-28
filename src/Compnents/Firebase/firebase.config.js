c54a348d2b964383265684aee19b78d924a359c3
Jeem312 <Shajidajeem312@gmail.com>
2024-04-28 19:32:51 +0600

    .env file

--- src/Compnents/Firebase/firebase.config.js
+++ src/Compnents/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPZu464oRAp9_LCEhkXgYgwHyBkMuKWzw",
-  authDomain: "assignment-10-b3b81.firebaseapp.com",
-  projectId: "assignment-10-b3b81",
-  storageBucket: "assignment-10-b3b81.appspot.com",
-  messagingSenderId: "17223270405",
-  appId: "1:17223270405:web:f6ef72307ecb0dc68b5c6c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain:process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
